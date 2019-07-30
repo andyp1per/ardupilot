a57a9be193be849ea8ea029fa57ed27419563b18
Andy Piper <github@andypiper.com>
2019-07-30 17:17:41 +0100

    ArduCopter: sample gyros at fast loop rate.

--- ArduCopter/ArduCopter.cpp
+++ ArduCopter/ArduCopter.cpp
@@ -149,7 +149,6 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
     SCHED_TASK_CLASS(DataFlash_Class,      &copter.DataFlash,           periodic_tasks, 400, 300),
 #endif
     SCHED_TASK_CLASS(AP_InertialSensor,    &copter.ins,                 periodic,       400,  50),
-    SCHED_TASK_CLASS(Analyse_Noise,        &copter.analyse_noise,       periodic,       400,  50),
     SCHED_TASK_CLASS(Analyse_Noise,        &copter.analyse_noise,       analyse,        400, 100),
     SCHED_TASK_CLASS(AP_Scheduler,         &copter.scheduler,           update_logging, 0.1,  75),
 #if RPM_ENABLED == ENABLED
@@ -262,6 +261,8 @@ void Copter::fast_loop()
     if (should_log(MASK_LOG_ANY)) {
         Log_Sensor_Health();
     }
+
+    analyse_noise.sample_gyros();
 }
 
 // rc_loops - reads user input from transmitter/receiver
