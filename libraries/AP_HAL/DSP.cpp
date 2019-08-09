7dcf51841e530a173e84f68d07013c342e4a816b
Andy Piper <github@andypiper.com>
2019-08-09 17:04:00 +0100

    AP_HAL: hardware abstraction for FFT

---
+++ libraries/AP_HAL/DSP.cpp
@@ -0,0 +1,36 @@
+/*
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Code by Andy Piper
+ */
+#include "AP_HAL.h"
+#include "DSP.h"
+
+using namespace AP_HAL;
+
+extern const AP_HAL::HAL &hal;
+
+DSP::FFTWindowState::FFTWindowState(uint16_t window_size, uint16_t sample_rate, uint8_t update_steps) 
+    : _update_steps(update_steps),
+    _window_size(window_size),
+    _bin_count(window_size / 2), 
+    _bin_resolution((float)sample_rate / _bin_count)
+{
+    _freq_bins = new float[(uint16_t)_bin_count + 1]; // includes DC ad Nyquist components
+}
+
+DSP::FFTWindowState::~FFTWindowState() 
+{
+    delete[] _freq_bins;
+}
