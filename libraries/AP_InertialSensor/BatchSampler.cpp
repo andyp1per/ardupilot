ef46e0eee62bf14038939fe03dbbdb0e1226dd8f
Andy Piper <github@andypiper.com>
2019-10-02 09:29:31 +0100

    Revert "AP_InertialSensor: correct batch sampler wrapping and concurrency"
    This reverts commit e47896d3dd5f5f66b0247c6bb2057b3cfa6dc340.

--- libraries/AP_InertialSensor/BatchSampler.cpp
+++ libraries/AP_InertialSensor/BatchSampler.cpp
@@ -30,13 +30,17 @@ const AP_Param::GroupInfo AP_InertialSensor::BatchSampler::var_info[] = {
     // @Description: Interval between pushing samples to the DataFlash log
     // @Units: ms
     // @Increment: 10
-    AP_GROUPINFO("BAT_LGIN", 4, AP_InertialSensor::BatchSampler, _push_interval_ms,   20),
+    AP_GROUPINFO("BAT_LGIN", 4, AP_InertialSensor::BatchSampler, push_interval_ms,   20),
+
+    // @Param: BAT_LGCT
+    // @DisplayName: logging count
+    // @Description: Number of samples to push to count every @PREFIX@BAT_LGIN
+    // @Increment: 1
+    AP_GROUPINFO("BAT_LGCT", 5, AP_InertialSensor::BatchSampler, samples_per_msg,   32),
 
     AP_GROUPEND
 };
 
-// This size is hardcoded in AP_Logger::Write_ISBD
-#define BATCH_SAMPLES_PER_MESSAGE 32
 
 extern const AP_HAL::HAL& hal;
 void AP_InertialSensor::BatchSampler::init()
@@ -48,38 +52,28 @@ void AP_InertialSensor::BatchSampler::init()
         return;
     }
 
-    // we assume the number of gyros and accels is the same, taking
-    // this minimum stops us doing bad things if that isn't true:
-    _instance_count = MIN(_imu._accel_count, _imu._gyro_count);
-    const uint8_t sensor_count = _instance_count * 2;
-    _required_count -= _required_count % BATCH_SAMPLES_PER_MESSAGE; // round down to nearest multiple of 32
+    _required_count -= _required_count % 32; // round down to nearest multiple of 32
 
-    const uint32_t total_allocation = 3 * _required_count * sizeof(uint16_t) * sensor_count;
-    gcs().send_text(MAV_SEVERITY_DEBUG, "INS: alloc %u bytes for ISB (free=%u)", (unsigned int)total_allocation, (unsigned int)hal.util->available_memory());
+    const uint32_t total_allocation = 3*_required_count*sizeof(uint16_t);
+    gcs().send_text(MAV_SEVERITY_DEBUG, "INS: alloc %u bytes for ISB (free=%u)", total_allocation, hal.util->available_memory());
 
-    for (uint8_t i = 0; i < sensor_count; i++) {
-        _data_x[i] = (int16_t*)calloc(_required_count, sizeof(int16_t));
-        _data_y[i] = (int16_t*)calloc(_required_count, sizeof(int16_t));
-        _data_z[i] = (int16_t*)calloc(_required_count, sizeof(int16_t));
-        _data_read_offset[i] = 0;
-        _data_write_offset[i] = 0;
-        if (_data_x[i] == nullptr || _data_y[i] == nullptr || _data_z[i] == nullptr) {
-            for (uint8_t j = 0; j <= i; j++) {
-                free(_data_x[j]);
-                free(_data_y[j]);
-                free(_data_z[j]);
-                _data_x[j] = nullptr;
-                _data_y[j] = nullptr;
-                _data_z[j] = nullptr;
-            }
-            gcs().send_text(MAV_SEVERITY_WARNING, "Failed to allocate %u bytes for IMU batch sampling", (unsigned int)total_allocation);
-            return;
-        }
+    data_x = (int16_t*)calloc(_required_count, sizeof(int16_t));
+    data_y = (int16_t*)calloc(_required_count, sizeof(int16_t));
+    data_z = (int16_t*)calloc(_required_count, sizeof(int16_t));
+    if (data_x == nullptr || data_y == nullptr || data_z == nullptr) {
+        free(data_x);
+        free(data_y);
+        free(data_z);
+        data_x = nullptr;
+        data_y = nullptr;
+        data_z = nullptr;
+        gcs().send_text(MAV_SEVERITY_WARNING, "Failed to allocate %u bytes for IMU batch sampling", total_allocation);
+        return;
     }
 
     rotate_to_next_sensor();
 
-    _initialised = true;
+    initialised = true;
 }
 
 void AP_InertialSensor::BatchSampler::periodic()
@@ -103,8 +97,8 @@ void AP_InertialSensor::BatchSampler::update_doing_sensor_rate_logging()
         _doing_sensor_rate_logging = false;
         return;
     }
-    const uint8_t bit = (1<<_instance);
-    switch (_type) {
+    const uint8_t bit = (1<<instance);
+    switch (type) {
     case IMU_SENSOR_TYPE_GYRO:
         _doing_sensor_rate_logging = _imu._gyro_sensor_rate_sampling_enabled & bit;
         break;
@@ -120,36 +114,41 @@ void AP_InertialSensor::BatchSampler::rotate_to_next_sensor()
         // should not have been called
         return;
     }
-    if ((1U<<_instance) > (uint8_t)_sensor_mask) {
+    if ((1U<<instance) > (uint8_t)_sensor_mask) {
         // should only ever happen if user resets _sensor_mask
-        _instance = 0;
+        instance = 0;
     }
 
-    if (_type == IMU_SENSOR_TYPE_ACCEL) {
+    if (type == IMU_SENSOR_TYPE_ACCEL) {
         // we have logged accelerometers, now log gyros:
-        _type = IMU_SENSOR_TYPE_GYRO;
-        _multiplier = _imu._gyro_raw_sampling_multiplier[_instance];
+        type = IMU_SENSOR_TYPE_GYRO;
+        multiplier = _imu._gyro_raw_sampling_multiplier[instance];
         update_doing_sensor_rate_logging();
-        _sensor_index = _instance + _instance_count;
         return;
     }
 
     // log for accel sensor:
-    _type = IMU_SENSOR_TYPE_ACCEL;
+    type = IMU_SENSOR_TYPE_ACCEL;
 
     // move to next IMU backend:
+
+    // we assume the number of gyros and accels is the same, taking
+    // this minimum stops us doing bad things if that isn't true:
+    const uint8_t _count = MIN(_imu._accel_count, _imu._gyro_count);
+
+    // find next backend instance to log:
     bool haveinstance = false;
-    for (uint8_t i = _instance + 1; i < _instance_count; i++) {
+    for (uint8_t i=instance+1; i<_count; i++) {
         if (_sensor_mask & (1U<<i)) {
-            _instance = i;
+            instance = i;
             haveinstance = true;
             break;
         }
     }
     if (!haveinstance) {
-        for (uint8_t i = 0; i <= _instance; i++) {
+        for (uint8_t i=0; i<=instance; i++) {
             if (_sensor_mask & (1U<<i)) {
-                _instance = i;
+                instance = i;
                 haveinstance = true;
                 break;
             }
@@ -160,31 +159,30 @@ void AP_InertialSensor::BatchSampler::rotate_to_next_sensor()
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
         abort();
 #endif
-        _instance = 0;
+        instance = 0;
         return;
     }
 
-    _sensor_index = _instance;
-    _multiplier = _imu._accel_raw_sampling_multiplier[_instance];
+    multiplier = _imu._accel_raw_sampling_multiplier[instance];
     update_doing_sensor_rate_logging();
 }
 
 void AP_InertialSensor::BatchSampler::push_data_to_log()
 {
-    if (!_initialised) {
+    if (!initialised) {
         return;
     }
     if (_sensor_mask == 0) {
         return;
     }
-    // calculate data to write taking into account wrap around
-    const uint16_t data_read_offset = _data_read_offset[_sensor_index];
-    const uint16_t num_samples = (_data_write_offset[_sensor_index] - data_read_offset + _required_count) % _required_count;
-    if (num_samples < BATCH_SAMPLES_PER_MESSAGE) {
+    if (data_write_offset - data_read_offset < samples_per_msg) {
         // insuffucient data to pack a packet
         return;
     }
-
+    if (AP_HAL::millis() - last_sent_ms < (uint16_t)push_interval_ms) {
+        // avoid flooding DataFlash's buffer
+        return;
+    }
     DataFlash_Class *dataflash = DataFlash_Class::instance();
     if (dataflash == nullptr) {
         // should not have been called
@@ -192,78 +190,73 @@ void AP_InertialSensor::BatchSampler::push_data_to_log()
     }
 
     // possibly send isb header:
-    if (!_isbh_sent && data_read_offset == 0) {
-        if (AP_HAL::millis() - _last_sent_ms < (uint16_t)_push_interval_ms) {
-            // avoid flooding DataFlash's buffer
-            return;
-        }
-        else {
-            // beginning of a new cycle
-            _last_sent_ms = AP_HAL::millis();
-        }
+    if (!isbh_sent && data_read_offset == 0) {
         float sample_rate = 0; // avoid warning about uninitialised values
-        switch(_type) {
+        switch(type) {
         case IMU_SENSOR_TYPE_GYRO:
-            sample_rate = _imu._gyro_raw_sample_rates[_instance];
+            sample_rate = _imu._gyro_raw_sample_rates[instance];
             if (_doing_sensor_rate_logging) {
-                sample_rate *= _imu._gyro_over_sampling[_instance];
+                sample_rate *= _imu._gyro_over_sampling[instance];
             }
             break;
         case IMU_SENSOR_TYPE_ACCEL:
-            sample_rate = _imu._accel_raw_sample_rates[_instance];
+            sample_rate = _imu._accel_raw_sample_rates[instance];
             if (_doing_sensor_rate_logging) {
-                sample_rate *= _imu._accel_over_sampling[_instance];
+                sample_rate *= _imu._accel_over_sampling[instance];
             }
             break;
         }
-        if (!dataflash->Log_Write_ISBH(_isb_seqnum,
-                                       _type,
-                                       _instance,
-                                       _multiplier,
+        if (!dataflash->Log_Write_ISBH(isb_seqnum,
+                                       type,
+                                       instance,
+                                       multiplier,
                                        _required_count,
-                                       _measurement_started_us,
+                                       measurement_started_us,
                                        sample_rate)) {
             // buffer full?
             return;
         }
-        _isbh_sent = true;
+        isbh_sent = true;
     }
     // pack and send a data packet:
-    if (!dataflash->Log_Write_ISBD(_isb_seqnum,
-                                   data_read_offset / BATCH_SAMPLES_PER_MESSAGE,
-                                   &_data_x[_sensor_index][data_read_offset],
-                                   &_data_y[_sensor_index][data_read_offset],
-                                   &_data_z[_sensor_index][data_read_offset])) {
+    if (!dataflash->Log_Write_ISBD(isb_seqnum,
+                                   data_read_offset/samples_per_msg,
+                                   &data_x[data_read_offset],
+                                   &data_y[data_read_offset],
+                                   &data_z[data_read_offset])) {
         // maybe later?!
         return;
     }
-
-    _data_read_offset[_sensor_index] += BATCH_SAMPLES_PER_MESSAGE;
-
-    if (_data_read_offset[_sensor_index] >= _required_count) {
+    data_read_offset += samples_per_msg;
+    last_sent_ms = AP_HAL::millis();
+    if (data_read_offset >= _required_count) {
         // that was the last one.  Clean up:
-        _data_read_offset[_sensor_index] = 0;
-        _isb_seqnum++;
-        _isbh_sent = false;
+        data_read_offset = 0;
+        isb_seqnum++;
+        isbh_sent = false;
         // rotate to next instance:
         rotate_to_next_sensor();
+        data_write_offset = 0; // unlocks writing process
     }
 }
 
-bool AP_InertialSensor::BatchSampler::should_log(uint8_t sensor_index)
+bool AP_InertialSensor::BatchSampler::should_log(uint8_t _instance, IMU_SENSOR_TYPE _type)
 {
     if (_sensor_mask == 0) {
         return false;
     }
-
-    if (!_initialised) {
+    if (!initialised) {
         return false;
     }
-    // if there nearly a full buffer of samples still to write then back off sampling
-    if (((_data_write_offset[sensor_index] - _data_read_offset[sensor_index] + _required_count) % _required_count) >= _required_count - 1) {
+    if (_instance != instance) {
         return false;
     }
-
+    if (_type != type) {
+        return false;
+    }
+    if (data_write_offset >= _required_count) {
+        return false;
+    }
     DataFlash_Class *dataflash = DataFlash_Class::instance();
     if (dataflash == nullptr) {
         return false;
@@ -275,38 +268,18 @@ bool AP_InertialSensor::BatchSampler::should_log(uint8_t sensor_index)
     return true;
 }
 
-// sample a sensor, called from the gyro thread so concurrency issues must be observed
-void AP_InertialSensor::BatchSampler::sample(uint8_t instance, AP_InertialSensor::IMU_SENSOR_TYPE type, uint64_t sample_us, const Vector3f &sensor_sample)
+void AP_InertialSensor::BatchSampler::sample(uint8_t _instance, AP_InertialSensor::IMU_SENSOR_TYPE _type, uint64_t sample_us, const Vector3f &_sample)
 {
-    uint8_t sensor_index = 0;
-    uint16_t multiplier = 0;
-
-    switch (type) {
-    case IMU_SENSOR_TYPE_ACCEL:
-        sensor_index = instance;
-        multiplier = _imu._accel_raw_sampling_multiplier[instance];
-        break;
-    case IMU_SENSOR_TYPE_GYRO:
-        sensor_index = instance + _instance_count;
-        multiplier = _imu._gyro_raw_sampling_multiplier[instance];
-        break;
-    }
-
-    if (!should_log(sensor_index)) {
+    if (!should_log(_instance, _type)) {
         return;
     }
-
-    if (_data_write_offset[sensor_index] == 0) {
-        _measurement_started_us = sample_us;
+    if (data_write_offset == 0) {
+        measurement_started_us = sample_us;
     }
 
-    const uint16_t data_write_offset = _data_write_offset[sensor_index];
-    _data_x[sensor_index][data_write_offset] = multiplier * sensor_sample.x;
-    _data_y[sensor_index][data_write_offset] = multiplier * sensor_sample.y;
-    _data_z[sensor_index][data_write_offset] = multiplier * sensor_sample.z;
+    data_x[data_write_offset] = multiplier*_sample.x;
+    data_y[data_write_offset] = multiplier*_sample.y;
+    data_z[data_write_offset] = multiplier*_sample.z;
 
-    // this assumes that we can write out sample packets as fast as we accumulate samples, we write 32 samples at a time
-    // which means the gyro loop rate can be 32 / sensors * 2 faster than the output rate, so 5x for 3 sensors
-    // periodic is run at 400Hz which gives a maximum backend sample rate of 2Khz
-    _data_write_offset[sensor_index] = (_data_write_offset[sensor_index] + 1) % _required_count;
+    data_write_offset++; // may unblock the reading process
 }
