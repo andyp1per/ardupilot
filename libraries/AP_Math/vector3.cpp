5305697d44c8b93c147be34d705c0b2b41b7d342
Andy Piper <github@andypiper.com>
2019-10-08 18:49:45 +0100

    AP_Math: make ROTATION_PITCH_7 pitch by 30 degrees for Gecko frame

--- libraries/AP_Math/vector3.cpp
+++ libraries/AP_Math/vector3.cpp
@@ -245,12 +245,9 @@ void Vector3<T>::rotate(enum Rotation rotation)
         return;
     }
     case ROTATION_PITCH_7: {
-        const float sin_pitch = 0.12186934340514748f; // sinf(pitch);
-        const float cos_pitch = 0.992546151641322f; // cosf(pitch);
-        float tmpx = x;
-        float tmpz = z;
-        x =  cos_pitch * tmpx + sin_pitch * tmpz;
-        z = -sin_pitch * tmpx + cos_pitch * tmpz;
+        tmp = x * 0.866025f + z * 0.5f;
+        z = -x * 0.5f + z * 0.866025f;
+        x = tmp;
         return;
     }
     case ROTATION_CUSTOM: // no-op; caller should perform custom rotations via matrix multiplication
