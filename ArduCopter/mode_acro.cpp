rmackay9/rmackay9-ardupilot

Copter: Update ACRO to use rate parameters and update expo function

ArduCopter/mode_acro.cpp
@@ -96,46 +96,34 @@ float ModeAcro::throttle_hover() const
 
 // get_pilot_desired_angle_rates - transform pilot's roll pitch and yaw input into a desired lean angle rates
 // returns desired angle rates in centi-degrees-per-second
-void ModeAcro::get_pilot_desired_angle_rates(int16_t roll_in, int16_t pitch_in, int16_t yaw_in, float &roll_out, float &pitch_out, float &yaw_out)
+void ModeAcro::get_pilot_desired_angle_rates(float roll_in, float pitch_in, float yaw_in, float &roll_out, float &pitch_out, float &yaw_out)
 {
+    // todo: provide normalised commands from RC library so this can be removed.
+    roll_in /= ROLL_PITCH_YAW_INPUT_MAX;
+    pitch_in /= ROLL_PITCH_YAW_INPUT_MAX;
+
     float rate_limit;
-    Vector3f rate_ef_level, rate_bf_level, rate_bf_request;
+    Vector3f rate_ef_level_cd, rate_bf_level_cd, rate_bf_request_cd;
 
     // apply circular limit to pitch and roll inputs
     float total_in = norm(pitch_in, roll_in);
 
-    if (total_in > ROLL_PITCH_YAW_INPUT_MAX) {
-        float ratio = (float)ROLL_PITCH_YAW_INPUT_MAX / total_in;
+    if (total_in > 1.0) {
+        float ratio = 1.0 / total_in;
         roll_in *= ratio;
         pitch_in *= ratio;
     }
 
-    // range check expo
-    g.acro_rp_expo = constrain_float(g.acro_rp_expo, -0.5f, 1.0f);
-    
     // calculate roll, pitch rate requests
-    if (is_zero(g.acro_rp_expo)) {
-        rate_bf_request.x = roll_in * g.acro_rp_p;
-        rate_bf_request.y = pitch_in * g.acro_rp_p;
-    } else {
-        // expo variables
-        float rp_in, rp_in3, rp_out;
+    
+    // roll expo
+    rate_bf_request_cd.x = g2.acro_rp_rate * 100.0 * input_expo(roll_in, g.acro_rp_expo);
 
-        // roll expo
-        rp_in = float(roll_in)/ROLL_PITCH_YAW_INPUT_MAX;
-        rp_in3 = rp_in*rp_in*rp_in;
-        rp_out = (g.acro_rp_expo * rp_in3) + ((1.0f - g.acro_rp_expo) * rp_in);
-        rate_bf_request.x = ROLL_PITCH_YAW_INPUT_MAX * rp_out * g.acro_rp_p;
+    // pitch expo
+    rate_bf_request_cd.y = g2.acro_rp_rate * 100.0 * input_expo(pitch_in, g.acro_rp_expo);
 
-        // pitch expo
-        rp_in = float(pitch_in)/ROLL_PITCH_YAW_INPUT_MAX;
-        rp_in3 = rp_in*rp_in*rp_in;
-        rp_out = (g.acro_rp_expo * rp_in3) + ((1.0f - g.acro_rp_expo) * rp_in);
-        rate_bf_request.y = ROLL_PITCH_YAW_INPUT_MAX * rp_out * g.acro_rp_p;
-    }
-
-    // calculate yaw rate request
-    rate_bf_request.z = get_pilot_desired_yaw_rate(yaw_in);
+    // yaw expo
+    rate_bf_request_cd.z = g2.acro_rp_rate * 100.0 * input_expo(yaw_in, g2.acro_y_expo);
 
     // calculate earth frame rate corrections to pull the copter back to level while in ACRO mode
 
@@ -146,65 +134,65 @@ void ModeAcro::get_pilot_desired_angle_rates(int16_t roll_in, int16_t pitch_in, 
 
         // Calculate trainer mode earth frame rate command for roll
         int32_t roll_angle = wrap_180_cd(att_target.x);
-        rate_ef_level.x = -constrain_int32(roll_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_roll;
+        rate_ef_level_cd.x = -constrain_int32(roll_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_roll;
 
         // Calculate trainer mode earth frame rate command for pitch
         int32_t pitch_angle = wrap_180_cd(att_target.y);
-        rate_ef_level.y = -constrain_int32(pitch_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_pitch;
+        rate_ef_level_cd.y = -constrain_int32(pitch_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_pitch;
 
         // Calculate trainer mode earth frame rate command for yaw
-        rate_ef_level.z = 0;
+        rate_ef_level_cd.z = 0;
 
         // Calculate angle limiting earth frame rate commands
         if (g.acro_trainer == (uint8_t)Trainer::LIMITED) {
             const float angle_max = copter.aparm.angle_max;
             if (roll_angle > angle_max){
-                rate_ef_level.x += sqrt_controller(angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max_cdss(), G_Dt);
+                rate_ef_level_cd.x += sqrt_controller(angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max_cdss(), G_Dt);
             }else if (roll_angle < -angle_max) {
-                rate_ef_level.x += sqrt_controller(-angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max_cdss(), G_Dt);
+                rate_ef_level_cd.x += sqrt_controller(-angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max_cdss(), G_Dt);
             }
 
             if (pitch_angle > angle_max){
-                rate_ef_level.y += sqrt_controller(angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
+                rate_ef_level_cd.y += sqrt_controller(angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
             }else if (pitch_angle < -angle_max) {
-                rate_ef_level.y += sqrt_controller(-angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
+                rate_ef_level_cd.y += sqrt_controller(-angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
             }
         }
 
         // convert earth-frame level rates to body-frame level rates
-        attitude_control->euler_rate_to_ang_vel(attitude_control->get_att_target_euler_cd()*radians(0.01f), rate_ef_level, rate_bf_level);
+        attitude_control->euler_rate_to_ang_vel(attitude_control->get_att_target_euler_cd() * radians(0.01f), rate_ef_level_cd, rate_bf_level_cd);
 
         // combine earth frame rate corrections with rate requests
         if (g.acro_trainer == (uint8_t)Trainer::LIMITED) {
-            rate_bf_request.x += rate_bf_level.x;
-            rate_bf_request.y += rate_bf_level.y;
-            rate_bf_request.z += rate_bf_level.z;
+            rate_bf_request_cd.x += rate_bf_level_cd.x;
+            rate_bf_request_cd.y += rate_bf_level_cd.y;
+            rate_bf_request_cd.z += rate_bf_level_cd.z;
         }else{
             float acro_level_mix = constrain_float(1-float(MAX(MAX(abs(roll_in), abs(pitch_in)), abs(yaw_in))/4500.0), 0, 1)*ahrs.cos_pitch();
 
-            // Scale leveling rates by stick input
-            rate_bf_level = rate_bf_level*acro_level_mix;
+            // Scale levelling rates by stick input
+            rate_bf_level_cd = rate_bf_level_cd * acro_level_mix;
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.x)-fabsf(rate_bf_level.x));
-            rate_bf_request.x += rate_bf_level.x;
-            rate_bf_request.x = constrain_float(rate_bf_request.x, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.x)-fabsf(rate_bf_level_cd.x));
+            rate_bf_request_cd.x += rate_bf_level_cd.x;
+            rate_bf_request_cd.x = constrain_float(rate_bf_request_cd.x, -rate_limit, rate_limit);
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.y)-fabsf(rate_bf_level.y));
-            rate_bf_request.y += rate_bf_level.y;
-            rate_bf_request.y = constrain_float(rate_bf_request.y, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.y)-fabsf(rate_bf_level_cd.y));
+            rate_bf_request_cd.y += rate_bf_level_cd.y;
+            rate_bf_request_cd.y = constrain_float(rate_bf_request_cd.y, -rate_limit, rate_limit);
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.z)-fabsf(rate_bf_level.z));
-            rate_bf_request.z += rate_bf_level.z;
-            rate_bf_request.z = constrain_float(rate_bf_request.z, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.z)-fabsf(rate_bf_level_cd.z));
+            rate_bf_request_cd.z += rate_bf_level_cd.z;
+            rate_bf_request_cd.z = constrain_float(rate_bf_request_cd.z, -rate_limit, rate_limit);
         }
     }
 
     // hand back rate request
-    roll_out = rate_bf_request.x;
-    pitch_out = rate_bf_request.y;
-    yaw_out = rate_bf_request.z;
+    roll_out = rate_bf_request_cd.x;
+    pitch_out = rate_bf_request_cd.y;
+    yaw_out = rate_bf_request_cd.z;
 }
 #endif

ArduCopter/mode_sport.cpp
@@ -34,8 +34,8 @@ void ModeSport::run()
     // get pilot's desired roll and pitch rates
 
     // calculate rate requests
-    float target_roll_rate = channel_roll->get_control_in() * g.acro_rp_p;
-    float target_pitch_rate = channel_pitch->get_control_in() * g.acro_rp_p;
+    float target_roll_rate = channel_roll->get_control_in() * g2.acro_rp_rate * 100.0 / ROLL_PITCH_YAW_INPUT_MAX;
+    float target_pitch_rate = channel_pitch->get_control_in() * g2.acro_rp_rate * 100.0 / ROLL_PITCH_YAW_INPUT_MAX;
 
     // get attitude targets
     const Vector3f att_target = attitude_control->get_att_target_euler_cd();
@@ -50,15 +50,15 @@ void ModeSport::run()
 
     const float angle_max = copter.aparm.angle_max;
     if (roll_angle > angle_max){
-        target_roll_rate +=  sqrt_controller(angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max_cdss(), G_Dt);
+        target_roll_rate +=  sqrt_controller(angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max_cdss(), G_Dt);
     }else if (roll_angle < -angle_max) {
-        target_roll_rate +=  sqrt_controller(-angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max_cdss(), G_Dt);
+        target_roll_rate +=  sqrt_controller(-angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max_cdss(), G_Dt);
     }
 
     if (pitch_angle > angle_max){
-        target_pitch_rate +=  sqrt_controller(angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
+        target_pitch_rate +=  sqrt_controller(angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
     }else if (pitch_angle < -angle_max) {
-        target_pitch_rate +=  sqrt_controller(-angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
+        target_pitch_rate +=  sqrt_controller(-angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max_cdss(), G_Dt);
     }
 
     // get pilot's desired yaw rate

andyp1per/ardupilot

ArduCopter/mode_acro.cpp
@@ -12,7 +12,7 @@ void ModeAcro::run()
 {
     // convert the input to the desired body frame rate
     float target_roll, target_pitch, target_yaw;
-    get_pilot_desired_angle_rates(channel_roll->get_control_in(), channel_pitch->get_control_in(), channel_yaw->get_control_in(), target_roll, target_pitch, target_yaw);
+    get_pilot_desired_angle_rates(channel_roll->norm_input_dz(), channel_pitch->norm_input_dz(), channel_yaw->norm_input_dz(), target_roll, target_pitch, target_yaw);
 
     if (!motors->armed()) {
         // Motors should be Stopped
@@ -94,48 +94,32 @@ float ModeAcro::throttle_hover() const
     return Mode::throttle_hover();
 }
 
-// get_pilot_desired_angle_rates - transform pilot's roll pitch and yaw input into a desired lean angle rates
-// returns desired angle rates in centi-degrees-per-second
-void ModeAcro::get_pilot_desired_angle_rates(int16_t roll_in, int16_t pitch_in, int16_t yaw_in, float &roll_out, float &pitch_out, float &yaw_out)
+// get_pilot_desired_angle_rates - transform pilot's normalised roll pitch and yaw input into a desired lean angle rates
+// inputs are -1 to 1 and the function returns desired angle rates in centi-degrees-per-second
+void ModeAcro::get_pilot_desired_angle_rates(float roll_in, float pitch_in, float yaw_in, float &roll_out, float &pitch_out, float &yaw_out)
 {
     float rate_limit;
-    Vector3f rate_ef_level, rate_bf_level, rate_bf_request;
+    Vector3f rate_ef_level_cd, rate_bf_level_cd, rate_bf_request_cd;
 
     // apply circular limit to pitch and roll inputs
     float total_in = norm(pitch_in, roll_in);
 
-    if (total_in > ROLL_PITCH_YAW_INPUT_MAX) {
-        float ratio = (float)ROLL_PITCH_YAW_INPUT_MAX / total_in;
+    if (total_in > 1.0) {
+        float ratio = 1.0 / total_in;
         roll_in *= ratio;
         pitch_in *= ratio;
     }
 
-    // range check expo
-    g.acro_rp_expo = constrain_float(g.acro_rp_expo, -0.5f, 1.0f);
-    
     // calculate roll, pitch rate requests
-    if (is_zero(g.acro_rp_expo)) {
-        rate_bf_request.x = roll_in * g.acro_rp_p;
-        rate_bf_request.y = pitch_in * g.acro_rp_p;
-    } else {
-        // expo variables
-        float rp_in, rp_in3, rp_out;
+    
+    // roll expo
+    rate_bf_request_cd.x = g2.acro_rp_rate * 100.0 * input_expo(roll_in, g.acro_rp_expo);
 
-        // roll expo
-        rp_in = float(roll_in)/ROLL_PITCH_YAW_INPUT_MAX;
-        rp_in3 = rp_in*rp_in*rp_in;
-        rp_out = (g.acro_rp_expo * rp_in3) + ((1.0f - g.acro_rp_expo) * rp_in);
-        rate_bf_request.x = ROLL_PITCH_YAW_INPUT_MAX * rp_out * g.acro_rp_p;
+    // pitch expo
+    rate_bf_request_cd.y = g2.acro_rp_rate * 100.0 * input_expo(pitch_in, g.acro_rp_expo);
 
-        // pitch expo
-        rp_in = float(pitch_in)/ROLL_PITCH_YAW_INPUT_MAX;
-        rp_in3 = rp_in*rp_in*rp_in;
-        rp_out = (g.acro_rp_expo * rp_in3) + ((1.0f - g.acro_rp_expo) * rp_in);
-        rate_bf_request.y = ROLL_PITCH_YAW_INPUT_MAX * rp_out * g.acro_rp_p;
-    }
-
-    // calculate yaw rate request
-    rate_bf_request.z = get_pilot_desired_yaw_rate(yaw_in);
+    // yaw expo
+    rate_bf_request_cd.z = g2.acro_y_rate * 100.0 * input_expo(yaw_in, g2.acro_y_expo);
 
     // calculate earth frame rate corrections to pull the copter back to level while in ACRO mode
 
@@ -146,65 +130,65 @@ void ModeAcro::get_pilot_desired_angle_rates(int16_t roll_in, int16_t pitch_in, 
 
         // Calculate trainer mode earth frame rate command for roll
         int32_t roll_angle = wrap_180_cd(att_target.x);
-        rate_ef_level.x = -constrain_int32(roll_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_roll;
+        rate_ef_level_cd.x = -constrain_int32(roll_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_roll;
 
         // Calculate trainer mode earth frame rate command for pitch
         int32_t pitch_angle = wrap_180_cd(att_target.y);
-        rate_ef_level.y = -constrain_int32(pitch_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_pitch;
+        rate_ef_level_cd.y = -constrain_int32(pitch_angle, -ACRO_LEVEL_MAX_ANGLE, ACRO_LEVEL_MAX_ANGLE) * g.acro_balance_pitch;
 
         // Calculate trainer mode earth frame rate command for yaw
-        rate_ef_level.z = 0;
+        rate_ef_level_cd.z = 0;
 
         // Calculate angle limiting earth frame rate commands
         if (g.acro_trainer == (uint8_t)Trainer::LIMITED) {
             const float angle_max = copter.aparm.angle_max;
             if (roll_angle > angle_max){
-                rate_ef_level.x += sqrt_controller(angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max(), G_Dt);
+                rate_ef_level_cd.x += sqrt_controller(angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max(), G_Dt);
             }else if (roll_angle < -angle_max) {
-                rate_ef_level.x += sqrt_controller(-angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max(), G_Dt);
+                rate_ef_level_cd.x += sqrt_controller(-angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max(), G_Dt);
             }
 
             if (pitch_angle > angle_max){
-                rate_ef_level.y += sqrt_controller(angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max(), G_Dt);
+                rate_ef_level_cd.y += sqrt_controller(angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max(), G_Dt);
             }else if (pitch_angle < -angle_max) {
-                rate_ef_level.y += sqrt_controller(-angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max(), G_Dt);
+                rate_ef_level_cd.y += sqrt_controller(-angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max(), G_Dt);
             }
         }
 
         // convert earth-frame level rates to body-frame level rates
-        attitude_control->euler_rate_to_ang_vel(attitude_control->get_att_target_euler_cd()*radians(0.01f), rate_ef_level, rate_bf_level);
+        attitude_control->euler_rate_to_ang_vel(attitude_control->get_att_target_euler_cd() * radians(0.01f), rate_ef_level_cd, rate_bf_level_cd);
 
         // combine earth frame rate corrections with rate requests
         if (g.acro_trainer == (uint8_t)Trainer::LIMITED) {
-            rate_bf_request.x += rate_bf_level.x;
-            rate_bf_request.y += rate_bf_level.y;
-            rate_bf_request.z += rate_bf_level.z;
+            rate_bf_request_cd.x += rate_bf_level_cd.x;
+            rate_bf_request_cd.y += rate_bf_level_cd.y;
+            rate_bf_request_cd.z += rate_bf_level_cd.z;
         }else{
             float acro_level_mix = constrain_float(1-float(MAX(MAX(abs(roll_in), abs(pitch_in)), abs(yaw_in))/4500.0), 0, 1)*ahrs.cos_pitch();
 
-            // Scale leveling rates by stick input
-            rate_bf_level = rate_bf_level*acro_level_mix;
+            // Scale levelling rates by stick input
+            rate_bf_level_cd = rate_bf_level_cd * acro_level_mix;
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.x)-fabsf(rate_bf_level.x));
-            rate_bf_request.x += rate_bf_level.x;
-            rate_bf_request.x = constrain_float(rate_bf_request.x, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.x)-fabsf(rate_bf_level_cd.x));
+            rate_bf_request_cd.x += rate_bf_level_cd.x;
+            rate_bf_request_cd.x = constrain_float(rate_bf_request_cd.x, -rate_limit, rate_limit);
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.y)-fabsf(rate_bf_level.y));
-            rate_bf_request.y += rate_bf_level.y;
-            rate_bf_request.y = constrain_float(rate_bf_request.y, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.y)-fabsf(rate_bf_level_cd.y));
+            rate_bf_request_cd.y += rate_bf_level_cd.y;
+            rate_bf_request_cd.y = constrain_float(rate_bf_request_cd.y, -rate_limit, rate_limit);
 
             // Calculate rate limit to prevent change of rate through inverted
-            rate_limit = fabsf(fabsf(rate_bf_request.z)-fabsf(rate_bf_level.z));
-            rate_bf_request.z += rate_bf_level.z;
-            rate_bf_request.z = constrain_float(rate_bf_request.z, -rate_limit, rate_limit);
+            rate_limit = fabsf(fabsf(rate_bf_request_cd.z)-fabsf(rate_bf_level_cd.z));
+            rate_bf_request_cd.z += rate_bf_level_cd.z;
+            rate_bf_request_cd.z = constrain_float(rate_bf_request_cd.z, -rate_limit, rate_limit);
         }
     }
 
     // hand back rate request
-    roll_out = rate_bf_request.x;
-    pitch_out = rate_bf_request.y;
-    yaw_out = rate_bf_request.z;
+    roll_out = rate_bf_request_cd.x;
+    pitch_out = rate_bf_request_cd.y;
+    yaw_out = rate_bf_request_cd.z;
 }
 #endif

ArduCopter/mode_sport.cpp
@@ -34,8 +34,8 @@ void ModeSport::run()
     // get pilot's desired roll and pitch rates
 
     // calculate rate requests
-    float target_roll_rate = channel_roll->get_control_in() * g.acro_rp_p;
-    float target_pitch_rate = channel_pitch->get_control_in() * g.acro_rp_p;
+    float target_roll_rate = channel_roll->get_control_in() * g2.acro_rp_rate * 100.0 / ROLL_PITCH_YAW_INPUT_MAX;
+    float target_pitch_rate = channel_pitch->get_control_in() * g2.acro_rp_rate * 100.0 / ROLL_PITCH_YAW_INPUT_MAX;
 
     // get attitude targets
     const Vector3f att_target = attitude_control->get_att_target_euler_cd();
@@ -50,19 +50,19 @@ void ModeSport::run()
 
     const float angle_max = copter.aparm.angle_max;
     if (roll_angle > angle_max){
-        target_roll_rate +=  sqrt_controller(angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max(), G_Dt);
+        target_roll_rate +=  sqrt_controller(angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max(), G_Dt);
     }else if (roll_angle < -angle_max) {
-        target_roll_rate +=  sqrt_controller(-angle_max - roll_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_roll_max(), G_Dt);
+        target_roll_rate +=  sqrt_controller(-angle_max - roll_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_roll_max(), G_Dt);
     }
 
     if (pitch_angle > angle_max){
-        target_pitch_rate +=  sqrt_controller(angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max(), G_Dt);
+        target_pitch_rate +=  sqrt_controller(angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max(), G_Dt);
     }else if (pitch_angle < -angle_max) {
-        target_pitch_rate +=  sqrt_controller(-angle_max - pitch_angle, g.acro_rp_p * 4.5, attitude_control->get_accel_pitch_max(), G_Dt);
+        target_pitch_rate +=  sqrt_controller(-angle_max - pitch_angle, g2.acro_rp_rate * 100.0 / ACRO_LEVEL_MAX_OVERSHOOT, attitude_control->get_accel_pitch_max(), G_Dt);
     }
 
     // get pilot's desired yaw rate
-    float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
+    float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
 
     // get pilot desired climb rate
     float target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());