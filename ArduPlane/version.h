bfa279daef7542a2d3765e4ecea5e9e8df12fd19
Andy Piper <github@andypiper.com>
2022-09-30 09:28:55 +0100

    Plane: change version to reflect Foxtech custom build

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.2.3"
+#define THISFIRMWARE "ArduPlane V4.2.3-Foxtech"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,3,FIRMWARE_VERSION_TYPE_OFFICIAL
