b4f4ae5cb2196fbf4233be178ae635ff2ad4a8a2
Andy Piper <github@andypiper.com>
2023-10-08 21:07:38 +0100

    AP_BLHeli: provide accessor for telemetry rate

--- libraries/AP_BLHeli/AP_BLHeli.h
+++ libraries/AP_BLHeli/AP_BLHeli.h
@@ -55,6 +55,7 @@ public:
 
     uint32_t get_bidir_dshot_mask() const { return channel_bidir_dshot_mask.get(); }
     uint8_t get_motor_poles() const { return motor_poles.get(); }
+    uint16_t get_telemetry_rate() const { return telem_rate.get(); }
 
     static AP_BLHeli *get_singleton(void) {
         return _singleton;
