06af854b60fef71a96a6f25d8f9fec7ba4cd1ede
Andy Piper <github@andypiper.com>
2023-12-06 09:09:44 +0000

    update mount_mode_switch.lua

--- scripts/mount_mode_switch.lua
+++ scripts/mount_mode_switch.lua
@@ -7,21 +7,35 @@ local servo9_function = Parameter("SERVO9_FUNCTION")
 local MAV_SEVERITY = {EMERGENCY=0, ALERT=1, CRITICAL=2, ERROR=3, WARNING=4, NOTICE=5, INFO=6, DEBUG=7}
 
 -- state
-local prev_pos = -1
+local prev_pos = rc:get_aux_cached(300)
+local fpv_locked
 
+function set_function(state)
+    if state then
+        fpv_locked = true
+        servo9_function:set(56)
+        gcs:send_text(MAV_SEVERITY.INFO, "FPV Locked Mode")
+    else
+        fpv_locked = false
+        servo9_function:set(13)
+        gcs:send_text(MAV_SEVERITY.INFO, "FPV Stabilize Mode")
+    end
+end
+
+-- set up the initial state
+set_function(rc:get_aux_cached(300) == AuxSwitchPos.LOW)
+
+-- run updates at 1Hz
 function update()
     local sw_pos = rc:get_aux_cached(300)
     if sw_pos ~= prev_pos then
         if sw_pos == AuxSwitchPos.LOW then
-            servo9_function:set(56)
-            gcs:send_text(MAV_SEVERITY.INFO, "FPV Locked Mode")
-        else
-            servo9_function:set(13)
-            gcs:send_text(MAV_SEVERITY.INFO, "FPV Stabilize Mode")
+            set_function(not fpv_locked)
         end
         prev_pos = sw_pos
     end
     return update, 1000
 end
 
+-- start after 5s
 return update, 5000
