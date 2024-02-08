52ef92123f546864ad375248dce4bc44b2d53f53
Andy Piper <github@andypiper.com>
2024-02-08 14:44:48 +0000

    RC_Channel: use fence enable_configured()

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -1062,7 +1062,7 @@ void RC_Channel::do_aux_function_fence(const AuxSwitchPos ch_flag)
         return;
     }
 
-    fence->enable(ch_flag == AuxSwitchPos::HIGH);
+    fence->enable_configured(ch_flag == AuxSwitchPos::HIGH);
 }
 #endif
 
