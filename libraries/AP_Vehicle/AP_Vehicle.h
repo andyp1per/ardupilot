6bcd96cc14f6b9431e158e9163b61e0cd2bc1a5d
Andrew Tridgell <andrew@tridgell.net>
2024-02-15 12:06:22 +1100

    AP_Vehicle: use update_dynamic_notch() directly in rate loop
    control notch update with using_rate_thread

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -442,6 +442,12 @@ protected:
     // Check if this mode can be entered from the GCS
     bool block_GCS_mode_change(uint8_t mode_num, const uint8_t *mode_list, uint8_t mode_list_length) const;
 
+#if AP_INERTIALSENSOR_HARMONICNOTCH_ENABLED
+    // update the harmonic notch
+    void update_dynamic_notch(AP_InertialSensor::HarmonicNotch &notch);
+#endif
+    bool using_rate_thread;
+
 private:
 
     // delay() callback that processing MAVLink packets
@@ -453,9 +459,6 @@ private:
 
     // update the harmonic notch for throttle based notch
     void update_throttle_notch(AP_InertialSensor::HarmonicNotch &notch);
-
-    // update the harmonic notch
-    void update_dynamic_notch(AP_InertialSensor::HarmonicNotch &notch);
 
     // run notch update at either loop rate or 200Hz
     void update_dynamic_notch_at_specified_rate();
@@ -487,6 +490,7 @@ private:
 
     // Bitmask of modes to disable from gcs
     AP_Int32 flight_mode_GCS_block;
+
 };
 
 namespace AP {
