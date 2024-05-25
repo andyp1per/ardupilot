18afa93b7b73b72392cc0f60053e74f981624b7b
Andy Piper <github@andypiper.com>
2024-05-25 09:30:00 +0100

    AP_InertialSensor: keep a record of the priamry gyro and use it
    rename get_primary_* to get_first_healthy_*
    use primary gyro allocation to enable and disable zero waits in IMU driver

--- libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor_Backend.cpp
@@ -212,25 +212,19 @@ void AP_InertialSensor_Backend::apply_gyro_filters(const uint8_t instance, const
     save_gyro_window(instance, gyro, filter_phase++);
 
     Vector3f gyro_filtered = gyro;
-#if AP_AHRS_ENABLED && (AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED || AP_INERTIALSENSOR_HARMONICNOTCH_ENABLED)
-    const uint8_t primary_gyro = AP::ahrs().get_primary_gyro_index();
-#endif
-
     // apply the harmonic notch filters
     for (auto &notch : _imu.harmonic_notches) {
         if (!notch.params.enabled()) {
             continue;
         }
         bool inactive = notch.is_inactive();
-#if AP_AHRS_ENABLED
         // by default we only run the expensive notch filters on the
         // currently active IMU we reset the inactive notch filters so
         // that if we switch IMUs we're not left with old data
         if (!notch.params.hasOption(HarmonicNotchFilterParams::Options::EnableOnAllIMUs) &&
-            instance != primary_gyro) {
+            instance != _imu._primary_gyro) {
             inactive = true;
         }
-#endif
         if (inactive) {
             // while inactive we reset the filter so when it activates the first output
             // will be the first input sample
@@ -257,8 +251,8 @@ void AP_InertialSensor_Backend::apply_gyro_filters(const uint8_t instance, const
         _imu._gyro_filtered[instance] = gyro_filtered;
     }
 
-#if AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED && AP_AHRS_ENABLED
-    if (_imu._cmutex != nullptr && instance == primary_gyro) {
+#if AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED
+    if (_imu._cmutex != nullptr && instance == _imu._primary_gyro) {
         /*
           tell the rate thread we have a new sample
         */
@@ -810,6 +804,12 @@ void AP_InertialSensor_Backend::update_gyro(uint8_t instance) /* front end */
             notch.update_params(instance, sensors_converging(), gyro_rate);
         }
     }
+<<<<<<< HEAD
+=======
+#endif
+
+    set_primary_gyro(_imu._primary_gyro);
+>>>>>>> c4f7eb6a26... AP_InertialSensor: keep a record of the priamry gyro and use it
 }
 
 /*
@@ -832,6 +832,8 @@ void AP_InertialSensor_Backend::update_accel(uint8_t instance) /* front end */
         _imu._accel_filter[instance].set_cutoff_frequency(_accel_raw_sample_rate(instance), _accel_filter_cutoff());
         _last_accel_filter_hz = _accel_filter_cutoff();
     }
+
+    set_primary_accel(_imu._primary_accel);
 }
 
 #if HAL_LOGGING_ENABLED
