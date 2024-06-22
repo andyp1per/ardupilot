296cd7fbb5b4a4fa452cb0e01a7d85a90de0184b
Andy Piper <github@andypiper.com>
2024-06-22 20:26:22 +0100

    Copter: migrate fast rate enablement to FSTRATE_ENABLE

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -642,11 +642,26 @@ private:
     };
 
 
-    enum class FlightOptions {
+    enum class FlightOptions : uint8_t {
         DISABLE_THRUST_LOSS_CHECK     = (1<<0),   // 1
         DISABLE_YAW_IMBALANCE_WARNING = (1<<1),   // 2
         RELEASE_GRIPPER_ON_THRUST_LOSS = (1<<2),  // 4
     };
+
+#if AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED
+    // type of fast rate attitude controller in operation
+    enum class FastRateType : uint8_t {
+        FAST_RATE_DISABLED            = 0,
+        FAST_RATE_DYNAMIC             = 1,
+        FAST_RATE_FIXED               = 2,
+    };
+
+    FastRateType get_fast_rate_type() const { return FastRateType(g2.att_enable.get()); }
+#endif
+    // returns true if option is enabled for this vehicle
+    bool option_is_enabled(FlightOption option) const {
+        return (g2.flight_options & uint32_t(option)) != 0;
+    }
 
     static constexpr int8_t _failsafe_priorities[] = {
                                                       (int8_t)FailsafeAction::TERMINATE,
