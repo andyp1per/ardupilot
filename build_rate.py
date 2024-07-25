ad4940466759d857a218158b5c96a26706e834ad
Andy Piper <github@andypiper.com>
2024-07-25 08:03:40 +0100

    add builds for Pixhawk6C, Pixhawk6X and CUAV v5 Nano

--- build_rate.py
+++ build_rate.py
@@ -23,18 +23,18 @@ def run_program(cmd_list):
 
 version='v1'
 
-for f in ["MambaH743v4", "MatekH743-bdshot"]:
+for f in ["Pixhawk6C-bdshot", "Pixhawk6X-bdshot", "CUAVv5Nano-bdshot"]:
     print("Building firmware %s" % f)
     run_program(["./waf", "configure", "--board", f])
     run_program(["./waf", "clean"])
     run_program(["./waf", "copter"])
-    os.makedirs('/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f), exist_ok=True)
-    shutil.copy('build/%s/bin/arducopter.apj' % f, '/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f))
-    shutil.copy('build/%s/bin/arducopter_with_bl.hex' % f, '/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f))
+    os.makedirs('/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f), exist_ok=True)
+    shutil.copy('build/%s/bin/arducopter.apj' % f, '/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f))
+    shutil.copy('build/%s/bin/arducopter_with_bl.hex' % f, '/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s' % (version, f))
 
     run_program(["./waf", "configure", "--board", f, "--enable-stats"])
     run_program(["./waf", "clean"])
     run_program(["./waf", "copter"])
-    os.makedirs('/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f), exist_ok=True)
-    shutil.copy('build/%s/bin/arducopter.apj' % f, '/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f))
-    shutil.copy('build/%s/bin/arducopter_with_bl.hex' % f, '/mnt/c/Users/micro/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f))
+    os.makedirs('/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f), exist_ok=True)
+    shutil.copy('build/%s/bin/arducopter.apj' % f, '/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f))
+    shutil.copy('build/%s/bin/arducopter_with_bl.hex' % f, '/mnt/c/Users/Andy/Dropbox/Public/firmware/fast_rate/%s/%s-stats' % (version, f))
