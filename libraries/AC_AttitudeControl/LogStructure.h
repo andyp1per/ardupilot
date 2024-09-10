ac445773d6db092cb2b4dda52d39b605b1973bcd
Andy Piper <github@andypiper.com>
2024-09-10 11:52:38 +0100

    AC_AttitudeControl: introduce ANG log message for high resolution attitude logging
    Move RATE message to AC_AttitudeControl_Logging.cpp

--- libraries/AC_AttitudeControl/LogStructure.h
+++ libraries/AC_AttitudeControl/LogStructure.h
@@ -3,7 +3,8 @@
 #include <AP_Logger/LogStructure.h>
 
 #define LOG_IDS_FROM_AC_ATTITUDECONTROL \
-    LOG_RATE_MSG,
+    LOG_RATE_MSG, \
+    LOG_ANG_MSG
 
 // @LoggerMessage: RATE
 // @Description: Desired and achieved vehicle attitude rates. Not logged in Fixed Wing Plane modes.
@@ -39,6 +40,30 @@ struct PACKED log_Rate {
     float   throttle_slew;
 };
 
+// @LoggerMessage: ANG
+// @Description: Attitude control attitude
+// @Field: TimeUS: Timestamp of the current Attitude loop
+// @Field: DesRoll: vehicle desired roll
+// @Field: Roll: achieved vehicle roll
+// @Field: DesPitch: vehicle desired pitch
+// @Field: Pitch: achieved vehicle pitch
+// @Field: DesYaw: vehicle desired yaw
+// @Field: Yaw: achieved vehicle yaw
+// @Field: Dt: attitude delta time
+struct PACKED log_ANG {
+    LOG_PACKET_HEADER;
+    uint64_t time_us;
+    float control_roll;
+    float roll;
+    float control_pitch;
+    float pitch;
+    float control_yaw;
+    float yaw;
+    float sensor_dt;
+};
+
 #define LOG_STRUCTURE_FROM_AC_ATTITUDECONTROL        \
     { LOG_RATE_MSG, sizeof(log_Rate), \
-        "RATE", "Qfffffffffffff",  "TimeUS,RDes,R,ROut,PDes,P,POut,YDes,Y,YOut,ADes,A,AOut,AOutSlew", "skk-kk-kk-oo--", "F?????????BB--" , true }
+        "RATE", "Qfffffffffffff",  "TimeUS,RDes,R,ROut,PDes,P,POut,YDes,Y,YOut,ADes,A,AOut,AOutSlew", "skk-kk-kk-oo--", "F?????????BB--" , true }, \
+    { LOG_ANG_MSG, sizeof(log_ANG),\
+        "ANG", "Qfffffff", "TimeUS,DesRoll,Roll,DesPitch,Pitch,DesYaw,Yaw,Dt", "sddddhhs", "F0000000" , true }
