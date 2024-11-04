141c0cfb73446ac30518817cec64c2b5cb7fa540
Andy Piper <github@andypiper.com>
2024-11-04 14:17:25 +0000

    AP_Scripting: better notch setting script

--- libraries/AP_Scripting/tests/static_notches.lua
+++ libraries/AP_Scripting/tests/static_notches.lua
@@ -1,9 +1,9 @@
-local PARAM_TABLE_KEY = 33
+local PARAM_TABLE_KEY = 35
 local PARAM_TABLE_PREFIX = "SNTCH"
 
 local notches = {}
 
-param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 5)
+param:add_table(PARAM_TABLE_KEY, PARAM_TABLE_PREFIX, 4)
 
 for idx = 1, 4, 1 do
     param:add_param(PARAM_TABLE_KEY, idx, idx .. "_FREQ", 0)
@@ -12,27 +12,30 @@ end
 
 
 function update()
-    ---@class HarmonicNotch_ud
-    local notch = ins:get_harmonic_notch(0)
+    for n = 0, 1, 1 do
+        ---@class HarmonicNotch_ud
+        local notch = ins:get_harmonic_notch(n)
 
-    if notch == nil then
-        return update, 100
-    end
-    local nfreqs = 0
-    for idx = 0, 4, 1 do
-        if notches[idx] ~= nil and notches[idx]:get() ~= nil then
-            local f = notches[idx]:get()
-            if f ~= 0 then
-                if f ~= notch:get_frequency(idx) then
-                    print("Notch " .. idx + 1 .. " set to " .. f .. " (was " .. notch:get_frequency(idx) .. ")")
-                    notch:set_frequency(idx, f)
+        if notch == nil then
+            goto continue
+        end
+        local nfreqs = 0
+        for idx = 0, 3, 1 do
+            if notches[idx] ~= nil and notches[idx]:get() ~= nil then
+                local f = notches[idx]:get()
+                if f ~= 0 then
+                    if f ~= notch:get_frequency(idx) then
+                        print("HNTC" .. n+1 .."(" .. idx + 1 .. ") set to " .. f .. " (was " .. notch:get_frequency(idx) .. ")")
+                        notch:set_frequency(idx, f)
+                    end
+                    nfreqs = nfreqs + 1
                 end
-                nfreqs = nfreqs + 1
             end
         end
+        notch:set_num_frequencies(nfreqs)
+        ::continue::
     end
-    notch:set_num_frequencies(nfreqs)
-    return update, 100
+    return update, 500
 end
 
 return update()
