e6711ad1777106fa534b07a0768556a1a6943704
Andy Piper <github@andypiper.com>
2025-05-08 10:12:05 +0100

    AP_HAL_ChibiOS: write out parameter page sections

--- libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
+++ libraries/AP_HAL_ChibiOS/hwdef/scripts/chibios_hwdef.py
@@ -949,7 +949,6 @@ class ChibiOSHWDef(hwdef.HWDef):
 #define STM32_ETH_BUFFERS_EXTERN
 
 ''')
-
         defines = self.get_mcu_config('DEFINES', False)
         if defines is not None:
             for d in defines.keys():
@@ -1057,6 +1056,9 @@ class ChibiOSHWDef(hwdef.HWDef):
                 # storage at end of flash - leave room
                 if offset > bl_offset:
                     flash_reserve_end = flash_size - offset
+            if self.is_bootloader_fw():
+                f.write('#define STORAGE_FLASH_START_KB %u\n' % self.get_flash_page_offset_kb(storage_flash_page))
+                f.write('#define STORAGE_FLASH_END_KB %u\n' % self.get_flash_page_offset_kb(storage_flash_page+2))
 
         crashdump_enabled = bool(self.intdefines.get('AP_CRASHDUMP_ENABLED', (flash_size >= 2048 and not self.is_bootloader_fw())))  # noqa
         # lets pick a flash sector for Crash log
