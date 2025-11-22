75b4352d32e253da0ed8ea1e7ffae7094ef9907c
Andy Piper <github@andypiper.com>
2025-11-22 20:21:55 +0000

    AP_RCProtocol: propagate IMU temperature data to external AHRS

--- libraries/AP_RCProtocol/AP_CRSF_Out.cpp
+++ libraries/AP_RCProtocol/AP_CRSF_Out.cpp
@@ -92,6 +92,10 @@ void AP_CRSF_Out::init()
     }
 
     _state = State::WAITING_FOR_RC_LOCK;
+
+#if AP_EXTERNAL_AHRS_CRSF_ENABLED
+    AP::externalAHRS().set_IMU_rate(1000000UL / _frame_interval_us);
+#endif
 }
 
 bool AP_CRSF_Out::do_status_update()
@@ -264,8 +268,6 @@ bool AP_CRSF_Out::decode_crsf_packet(AP_CRSF_Protocol::Frame& _frame)
     hal.console->printf("\n");
 #endif
 
-    Vector3f acc, gyro;
-
     switch (_frame.type) {
         case AP_CRSF_Protocol::FrameType::CRSF_FRAMETYPE_COMMAND: {
             const AP_CRSF_Protocol::CommandFrame* cmd = (const AP_CRSF_Protocol::CommandFrame*)_frame.payload;
@@ -294,19 +296,21 @@ bool AP_CRSF_Out::decode_crsf_packet(AP_CRSF_Protocol::Frame& _frame)
             send_device_info();
             break;
 
-        case AP_CRSF_Protocol::FrameType::CRSF_FRAMETYPE_ACCGYRO:
-            if (AP_CRSF_Protocol::process_accgyro_frame((AP_CRSF_Protocol::AccGyroFrame*)_frame.payload, acc, gyro)) {
+        case AP_CRSF_Protocol::FrameType::CRSF_FRAMETYPE_ACCGYRO: {
+            Vector3f acc, gyro;
+            float gyro_temp;
+            if (AP_CRSF_Protocol::process_accgyro_frame((AP_CRSF_Protocol::AccGyroFrame*)_frame.payload, acc, gyro, gyro_temp)) {
                 // Pass the decoded IMU data to the external AHRS CRSF module
 #if AP_EXTERNAL_AHRS_CRSF_ENABLED
                 AP_ExternalAHRS_CRSF* crsf_ahrs = AP::external_ahrs_crsf();
                 if (crsf_ahrs != nullptr) {
                     // Pass this instance's index for filtering in the AHRS backend
-                    crsf_ahrs->handle_acc_gyro_frame(_instance_idx, acc, gyro);
+                    crsf_ahrs->handle_acc_gyro_frame(_instance_idx, acc, gyro, gyro_temp);
                 }
 #endif
             }
             break;
-
+        }
         default:
             break;
     }
