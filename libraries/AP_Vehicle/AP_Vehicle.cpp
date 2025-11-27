f82bc18b79b4bcb7ce9ee1cb5a2df815a6d15012
Andy Piper <github@andypiper.com>
2025-11-27 15:15:20 +0000

    AP_Vehicle: manager CRSF out manager rather than individual instances

--- libraries/AP_Vehicle/AP_Vehicle.cpp
+++ libraries/AP_Vehicle/AP_Vehicle.cpp
@@ -292,8 +292,8 @@ const AP_Param::GroupInfo AP_Vehicle::var_info[] = {
 
 #if AP_CRSF_OUT_ENABLED
     // @Group: CRSF
-    // @Path: ../AP_RCProtocol/AP_CRSF_Out.cpp
-    AP_SUBGROUPINFO(crsf_out, "CRSF_OUT_", 33, AP_Vehicle, AP_CRSF_Out),
+    // @Path: ../AP_RCProtocol/AP_CRSF_OutManager.cpp
+    AP_SUBGROUPINFO(crsf_out, "CRSF_OUT_", 33, AP_Vehicle, AP_CRSF_OutManager),
 #endif
 
 AP_GROUPEND
@@ -701,6 +701,9 @@ const AP_Scheduler::Task AP_Vehicle::scheduler_tasks[] = {
 #endif
 #if AP_ARMING_ENABLED
     SCHED_TASK(update_arming,          1,     50, 253),
+#endif
+#if AP_RCPROTOCOL_ENABLED
+    SCHED_TASK_CLASS(AP_CRSF_OutManager,  &vehicle.crsf_out, init,                      1, 100, 253),
 #endif
 };
 
