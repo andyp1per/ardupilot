69dea1b6d1e80ff51e65fca1fb364947c26ad7cb
Andy Piper <github@andypiper.com>
2025-11-28 18:19:27 +0000

    AP_RCProtocol: normalize CRSF output thread to regular beats and only send rc frames at 250Hz

--- libraries/AP_RCProtocol/AP_CRSF_Out.cpp
+++ libraries/AP_RCProtocol/AP_CRSF_Out.cpp
@@ -51,7 +51,7 @@ AP_CRSF_Out* AP_CRSF_Out::_singleton;
 extern const AP_HAL::HAL& hal;
 
 AP_CRSF_Out::AP_CRSF_Out(AP_HAL::UARTDriver* uart, uint8_t instance, AP_CRSF_OutManager& frontend) :
-    _instance_idx(instance), _frontend(frontend)
+    _instance_idx(instance), _uart(uart), _frontend(frontend)
 {
     if (_singleton == nullptr) {
         _singleton = this;
@@ -378,16 +378,6 @@ void AP_CRSF_Out::send_rc_frame()
         }
     }
 
-#ifdef CRSF_RCOUT_DEBUG
-    const uint32_t now_ms = AP_HAL::millis();
-    if (now_ms - last_update_debug_ms > 1000) {
-        debug_rcout("Updating channels @%u(%u)Hz. CH1=%u CH2=%u CH3=%u", unsigned(num_frames), unsigned(_frontend._rate_hz.get()),
-                    unsigned(channels[0]), unsigned(channels[1]), unsigned(channels[2]));
-        last_update_debug_ms = now_ms;
-        num_frames = 0;
-    }
-    num_frames++;
-#endif
     AP_CRSF_Protocol::Frame frame {};
 
     frame.device_address = DeviceAddress::CRSF_ADDRESS_SYNC_BYTE;
@@ -456,6 +446,10 @@ void AP_CRSF_Out::send_heartbeat()
     _crsf_port->write_frame(&frame);
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4242350e05 (AP_RCProtocol: normalize CRSF output thread to regular beats and only send rc frames at 250Hz)
 namespace AP {
     AP_CRSF_Out* crsf_out() {
         return AP_CRSF_Out::get_singleton();
