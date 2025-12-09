1c7a7dec32e02b7a3b2a6520a9bb15852e7e3ff2
Tamas Nepusz <ntamas@gmail.com>
2025-12-09 14:38:30 +0100

    refactor: move timing-related functions to a separate file

--- libraries/AC_DroneShowManager/AC_DroneShowManager.cpp
+++ libraries/AC_DroneShowManager/AC_DroneShowManager.cpp
@@ -3,7 +3,6 @@
 #include <sys/types.h>
 
 #include <AP_Filesystem/AP_Filesystem.h>
-#include <AP_GPS/AP_GPS.h>
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Logger/AP_Logger.h>
 #include <AP_Math/AP_Math.h>
@@ -458,85 +457,6 @@ void AC_DroneShowManager::get_distance_from_desired_position(Vector3f& vec) cons
     }
 }
 
-// returns the elapsed time since the start of the show, in microseconds
-int64_t AC_DroneShowManager::get_elapsed_time_since_start_usec() const
-{
-    uint64_t now, reference, diff;
-    
-    // AP::gps().time_epoch_usec() is smart enough to handle the case when
-    // the GPS fix was lost so no need to worry about loss of GPS fix here.
-    if (uses_gps_time_for_show_start()) {
-        now = AP::gps().time_epoch_usec();
-        reference = _start_time_unix_usec;
-    } else {
-        now = AP_HAL::micros64();
-        reference = _start_time_on_internal_clock_usec;
-    }
-
-    if (reference > 0) {
-        if (reference > now) {
-            diff = reference - now;
-            if (diff < INT64_MAX) {
-                return -diff;
-            } else {
-                return INT64_MIN;
-            }
-        } else if (reference < now) {
-            diff = now - reference;
-            if (diff < INT64_MAX) {
-                return diff;
-            } else {
-                return INT64_MAX;
-            }
-        } else {
-            return 0;
-        }
-    } else {
-        return INT64_MIN;
-    }
-}
-
-int32_t AC_DroneShowManager::get_elapsed_time_since_start_msec() const
-{
-    int64_t elapsed_usec = get_elapsed_time_since_start_usec();
-
-    // Using -INFINITY here can lead to FPEs on macOS in the SITL simulator
-    // when compiling in release mode, hence we use a large negative number
-    // representing one day
-    if (elapsed_usec <= -86400000000) {
-        return -86400000;
-    } else if (elapsed_usec >= 86400000000) {
-        return 86400000;
-    } else {
-        return static_cast<int32_t>(elapsed_usec / 1000);
-    }
-}
-
-float AC_DroneShowManager::get_elapsed_time_since_start_sec() const
-{
-    int64_t elapsed_usec = get_elapsed_time_since_start_usec();
-
-    // Using -INFINITY here can lead to FPEs on macOS in the SITL simulator
-    // when compiling in release mode, hence we use a large negative number
-    // representing one day
-    return elapsed_usec == INT64_MIN ? -86400 : static_cast<float>(elapsed_usec / 1000) / 1000.0f;
-}
-
-int64_t AC_DroneShowManager::get_time_until_start_usec() const
-{
-    return -get_elapsed_time_since_start_usec();
-}
-
-float AC_DroneShowManager::get_time_until_start_sec() const
-{
-    return -get_elapsed_time_since_start_sec();
-}
-
-float AC_DroneShowManager::get_time_until_landing_sec() const
-{
-    return get_time_until_start_sec() + get_relative_landing_time_sec();
-}
-
 void AC_DroneShowManager::notify_drone_show_mode_initialized()
 {
     _cancel_requested = false;
@@ -740,15 +660,6 @@ bool AC_DroneShowManager::_is_close_to_position(
     }
 
     return true;
-}
-
-bool AC_DroneShowManager::_is_gps_time_ok() const
-{
-    // AP::gos().time_week() starts from zero and gets set to a non-zero value
-    // when we start receiving full time information from the GPS. It may happen
-    // that the GPS subsystem receives iTOW information from the GPS module but
-    // no week number; we deem this unreliable so we return false in this case.
-    return AP::gps().time_week() > 0;
 }
 
 void AC_DroneShowManager::_update_preflight_check_result(bool force)
